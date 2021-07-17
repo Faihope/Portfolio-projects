77a9c1a92d85ca15628f6862078f8dd0837998ad
Fai <chepngetichfaith46@gmail.com>
2021-07-17 12:37:00 +0300

    Add base.html file for template inheritance and navbar

--- projectprofiles/asgi.py
+++ projectprofiles/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for projectprofile project.
+ASGI config for projectprofiles project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectprofile.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectprofiles.settings')
 
 application = get_asgi_application()
--- projectprofiles/wsgi.py
+++ projectprofiles/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for projectprofile project.
+WSGI config for projectprofiles project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectprofile.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectprofiles.settings')
 
 application = get_wsgi_application()
